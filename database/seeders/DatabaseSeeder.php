82a35a35d00a71958bba19824ce6a80e4a3dc11e
hkndeveloper <hk9916971omg@gmail.com>
2026-04-29 14:56:24 +0300

    database alşskd

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             ProjectSeeder::class,
             SystemSettingsSeeder::class,
+            DemoProjectRoleDataSeeder::class,
         ]);
     }
 }
